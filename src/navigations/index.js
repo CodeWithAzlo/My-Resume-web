0dc1374b5bc2cb32d56bc687cfb873eb697b6a30
Azlan Shah <codewithazlo@gmail.com>
2023-11-10 16:20:02 +0500

    my resume app hosting

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
           element:<About/>
         },
         {
-          path:"  ",
+          path:"/contact",
           element:<Contact/>
         },
       ]
